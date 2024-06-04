3a228787bc0cbcfa3363e02627d4d7cb59c6f171
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-06-03 20:15:15 -0500

    Finished JwtTokenValidator

---
+++ src/main/java/com/devryan/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.devryan.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="fjasfljaslfja sklfjaksljflajflksjf laksjflkajsflksjflksjflksjfd;lksj";
+    public static final String JWT_HEADER="Authorization";
+}
