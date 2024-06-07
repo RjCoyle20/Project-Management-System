4fabbc25b1d2c1bf63c702d19123e56925fe6b64
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-06-06 19:19:18 -0500

    User model created

---
+++ src/main/java/com/devryan/model/User.java
@@ -0,0 +1,26 @@
+package com.devryan.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.sql.Array;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+
+    private List<Issue>assignedIssues = new Array();
+    private int projectSize;
+
+}
